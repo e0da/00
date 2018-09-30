b3992049963ed8cccd92fc8614bddffe0b131cb2
Justin Force <justin.force@gmail.com>
2018-09-29 17:21:47 -0700

    Fix emscripten loop compat
    Emscripten has to hook into your main loop a little differently to play
    nice with the browser. It's actually pretty awesome.
    
    https://kripken.github.io/emscripten-site/docs/porting/emscripten-runtime-environment.html#implementing-an-asynchronous-main-loop-in-c-c
    
    This moved me to extract all of the shared variables into globals. Eh.
    Eventually all storage will be a single global object, so it's not
    actually that gross.

--- 00.c
+++ 00.c
@@ -3,74 +3,84 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 #define WINDOW_TITLE "00: o hai windoe"
 #define WIDTH 1024
 #define HEIGHT 768
+#define FPS 60
 
-void init(SDL_Window **window, SDL_Surface **surface, bool *running);
-void iterate(bool *running, SDL_Surface *surface, SDL_Window *window);
-void quit(SDL_Window **window);
+static SDL_Window *window;
+static SDL_Surface *surface;
+static bool running;
 
-void handle_events(bool *running);
-void draw_background(SDL_Surface *surface);
+void init(void);
+void iterate(void);
+void quit(void);
+
+void handle_events(void);
+void draw_background(void);
 
 void warn_if_sdl_error(const char *warning, bool condition);
 
 int main() {
-  SDL_Window *window;
-  SDL_Surface *surface;
-  bool running = false;
+  init();
 
-  init(&window, &surface, &running);
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(iterate, FPS, 1);
+#else
   while (running) {
-    iterate(&running, surface, window);
+    iterate();
+    SDL_Delay(1000 / FPS);
   }
-  quit(&window);
+#endif
+
+  quit();
 
   return 0;
 }
 
-void init(SDL_Window **window, SDL_Surface **surface, bool *running) {
+void init() {
   bool init = !SDL_Init(SDL_INIT_VIDEO); // SDL_Init returns 0 on success
   warn_if_sdl_error("SDL_Init failed", init);
 
-  *window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
-                             SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
-                             SDL_WINDOW_SHOWN);
-  warn_if_sdl_error("SDL_CreateWindow failed", *window);
+  window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
+                            SDL_WINDOW_SHOWN);
+  warn_if_sdl_error("SDL_CreateWindow failed", window);
 
-  *surface = SDL_GetWindowSurface(*window);
-  warn_if_sdl_error("SDL_GetWindowSurface failed", *surface);
+  surface = SDL_GetWindowSurface(window);
+  warn_if_sdl_error("SDL_GetWindowSurface failed", surface);
 
-  if (init && *window && *surface) {
-    *running = true;
-  }
+  running = init && window && surface;
 }
 
-void iterate(bool *running, SDL_Surface *surface, SDL_Window *window) {
-  handle_events(running);
-  draw_background(surface);
+void iterate() {
+  handle_events();
+  draw_background();
   SDL_UpdateWindowSurface(window);
 }
 
-void quit(SDL_Window **window) {
-  SDL_DestroyWindow(*window);
+void quit() {
+  SDL_DestroyWindow(window);
   SDL_Quit();
 }
 
-void handle_events(bool *running) {
+void handle_events() {
   SDL_Event event;
   SDL_PollEvent(&event);
   switch (event.type) {
   case SDL_QUIT:
-    *running = false;
+    running = false;
     break;
   default:
     break;
   }
 }
 
-void draw_background(SDL_Surface *surface) {
+void draw_background() {
   const unsigned int cyan = SDL_MapRGB(surface->format, 0x00, 0xff, 0xff);
   SDL_FillRect(surface, NULL, cyan);
 }
